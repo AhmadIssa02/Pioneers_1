2138034e2f35ffc5875e36babc57a25e950fc632
Ahmad Issa <104472580+AhmadIssa02@users.noreply.github.com>
2025-03-09 11:40:21 +0300

    Edit Receipt

--- Bank/lib/login/widgets/bottom_sheet/receipt_bottom_sheet.dart
+++ Bank/lib/login/widgets/bottom_sheet/receipt_bottom_sheet.dart
@@ -4,8 +4,14 @@ import 'package:new_app/model/item_model.dart';
 class ReceiptBottomSheet extends StatefulWidget {
   final List<ItemModel> itemsList;
   final Function(ItemModel) onDelete;
+  final Function(ItemModel) onRemove;
+  final Function(ItemModel) onAdd;
   const ReceiptBottomSheet(
-      {super.key, required this.itemsList, required this.onDelete});
+      {super.key,
+      required this.itemsList,
+      required this.onDelete,
+      required this.onRemove,
+      required this.onAdd});
   @override
   State<ReceiptBottomSheet> createState() => _ReceiptBottomSheetState();
 }
@@ -29,6 +35,7 @@ class _ReceiptBottomSheetState extends State<ReceiptBottomSheet> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              SizedBox(width: 4),
               SizedBox(
                 width: MediaQuery.of(context).size.width / 5,
                 child: Text("Item Name"),
@@ -47,6 +54,11 @@ class _ReceiptBottomSheetState extends State<ReceiptBottomSheet> {
                 width: MediaQuery.of(context).size.width / 5,
                 child: Text("Total Price"),
               ),
+              Expanded(child: SizedBox()),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 10,
+                child: Icon(Icons.receipt),
+              ),
             ],
           ),
         ),
@@ -59,6 +71,7 @@ class _ReceiptBottomSheetState extends State<ReceiptBottomSheet> {
                   color: Colors.white38,
                   child: Row(
                     children: [
+                      SizedBox(width: 4),
                       SizedBox(
                         width: MediaQuery.of(context).size.width / 5,
                         child: Text(widget.itemsList[index].name),
@@ -68,10 +81,42 @@ class _ReceiptBottomSheetState extends State<ReceiptBottomSheet> {
                           width: MediaQuery.of(context).size.width / 5,
                           child: Text("${widget.itemsList[index].price}")),
                       Expanded(child: SizedBox()),
+                      //TODO + - (and at 0 delete ) use callback
                       SizedBox(
                         width: MediaQuery.of(context).size.width / 5,
-                        //TODO + - (and at 0 delete ) use callback
-                        child: Text("${widget.itemsList[index].quantity}"),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            InkWell(
+                                onTap: () {
+                                  if (widget.itemsList[index].quantity == 1) {
+                                    widget.onDelete(widget.itemsList[index]);
+                                    widget.itemsList.removeAt(index);
+                                  } else {
+                                    widget.onRemove(widget.itemsList[index]);
+                                  }
+                                  setState(() {});
+                                },
+                                child: Icon(
+                                  Icons.remove,
+                                  size: 18,
+                                )),
+                            Text(
+                              "${widget.itemsList[index].quantity}",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            InkWell(
+                              onTap: () {
+                                widget.onAdd(widget.itemsList[index]);
+                                setState(() {});
+                              },
+                              child: Icon(
+                                Icons.add,
+                                size: 18,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                       Expanded(child: SizedBox()),
                       SizedBox(
@@ -81,7 +126,7 @@ class _ReceiptBottomSheetState extends State<ReceiptBottomSheet> {
                       ),
                       Expanded(child: SizedBox()),
                       SizedBox(
-                        width: MediaQuery.of(context).size.width / 5,
+                        width: MediaQuery.of(context).size.width / 10,
                         child: IconButton(
                           onPressed: () {
                             widget.onDelete(widget.itemsList[index]);
